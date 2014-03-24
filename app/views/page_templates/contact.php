54421c0b08a31db8fcdd817649f7ddecf6a3e85d
Freeeed <friedrich@ueberreiter.com>
2014-03-24 12:34:25 +0100

    Added some programs to views

--- app/views/page_templates/contact.php
+++ app/views/page_templates/contact.php
@@ -4,11 +4,3 @@
 <div hcms-form="contact">
 	
 </div>
-
-<?php echo ""; /*Program::build('t=menu ns=global fake');*/ ?>
-
-<?php echo ""; /*Program::build('t=text fake');*/ ?>
-
-<?php echo ""; /*Program::build('t=form fake');*/ ?>
-
-<?php echo ""; /*Program::build('t=footer ns=global fake');*/ ?>
--- app/views/page_templates/default.php
+++ app/views/page_templates/default.php
@@ -2,5 +2,13 @@
 <h1>Default</h1>
 
 <div pcms-text="global">
-	Ich bin der Standardtext!
+    Ich bin der Standardtext!
+</div>
+
+<div pcms-cool-ns-program="p1">
+    Cooles Programm p1
+</div>
+
+<div pcms-cool-ns-program="p2">
+    Cooles Programm p2
 </div>
