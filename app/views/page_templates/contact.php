8714b0710044e95e15929cce2410890b3c7af92e
Freeeed <friedrich@ueberreiter.com>
2014-03-24 09:11:11 +0100

    Added some test view files

---
+++ app/views/page_templates/contact.php
@@ -0,0 +1,14 @@
+
+<h1>Contact</h1>
+
+<div hcms-form="contact">
+	
+</div>
+
+<?php echo ""; /*Program::build('t=menu ns=global fake');*/ ?>
+
+<?php echo ""; /*Program::build('t=text fake');*/ ?>
+
+<?php echo ""; /*Program::build('t=form fake');*/ ?>
+
+<?php echo ""; /*Program::build('t=footer ns=global fake');*/ ?>
