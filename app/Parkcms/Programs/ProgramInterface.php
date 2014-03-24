9d4a2a8d1ed9844853d85c0b7137e90b065383d3
Freeeed <friedrich@ueberreiter.com>
2014-03-24 12:34:09 +0100

    Fixed space indent

--- app/Parkcms/Programs/ProgramInterface.php
+++ app/Parkcms/Programs/ProgramInterface.php
@@ -3,19 +3,19 @@
 namespace Parkcms\Programs;
 
 interface ProgramInterface {
-	
-	/**
-	 * initialize the program and returns true at success
-	 * @param  string $identifier
-	 * @param  array  $params
-	 * @return true|false
-	 */
-	public function initialize($identifier, array $params);
-	
-	/**
-	 * renders the program and returns the result
-	 * @return string
-	 */
-	public function render();
-	
+    
+    /**
+     * initialize the program and returns true at success
+     * @param  string $identifier
+     * @param  array  $params
+     * @return true|false
+     */
+    public function initialize($identifier, array $params);
+    
+    /**
+     * renders the program and returns the result
+     * @return string
+     */
+    public function render();
+    
 }
