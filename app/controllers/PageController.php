e9b0fcdb919f1c812094c66c6c1aeabb19b2a434
Freeeed <friedrich@ueberreiter.com>
2014-03-24 09:13:58 +0100

    Added PageController with method showPage()
    showPage($route) renders the page for the given route and returns the
    html page

---
+++ app/controllers/PageController.php
@@ -0,0 +1,42 @@
+<?php
+
+use Hardcore\Models\Page;
+
+use Parkcms\Template\AttributeParser as Parser;
+
+class PageController extends BaseController {
+	
+	protected $parser;
+	
+	public function __construct(Parser $parser) {
+		$this->parser = $parser;
+	}
+	
+	public function showPage($route)
+	{
+		$lang = 'de'; // magic to get lang
+		
+		$root = Parkcms\Models\Page::roots()->where('title', $lang)->first();
+		
+		$page = $root->children()->where('alias', $route)->first();
+		
+		if($page !== null) {
+			// var_dump($this->_route->page->route);
+			
+			// var_dump($this->_route->params());
+			
+			$view = View::make('layout')->nest('body', 'page_templates.' . $page->template)->render();
+			
+			$this->parser->setSource($view);
+			
+			$this->parser->pushHandler(function($type, $identifier, $data, $nodeValue) {
+				if($program = ProgramManager::load($type, $identifier, $data)) {
+					return $program->content();
+				}
+				return null;
+			});
+			
+			return $this->parser->parse();
+		}
+	}
+}
