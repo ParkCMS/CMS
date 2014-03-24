c8a788e9b7f0cf2c93cc935872dd9957db8bbb28
Freeeed <friedrich@ueberreiter.com>
2014-03-24 13:56:36 +0100

    Added test data

--- app/Parkcms/Programs/Cool/Ns/Program.php
+++ app/Parkcms/Programs/Cool/Ns/Program.php
@@ -3,8 +3,19 @@
 namespace Parkcms\Programs\Cool\Ns;
 
 use Parkcms\Programs\ProgramInterface;
+use Parkcms\Context;
+
+use Illuminate\Filesystem\Filesystem as File;
 
 class Program implements ProgramInterface {
+
+    protected $context;
+
+    public function __construct(Context $context, File $file) {
+        $this->context = $context;
+        $this->file = $file;
+    }
+
     /**
      * initialize the program and returns true at success
      * @param  string $identifier
@@ -12,14 +23,27 @@ class Program implements ProgramInterface {
      * @return true|false
      */
     public function initialize($identifier, array $params) {
-        return $identifier == "p1";
+
+        if(strpos($identifier, "global") === 0) {
+            $this->content = $this->file->get(__DIR__ . '/' . $identifier . '.txt');
+            return true;
+        }
+
+        $path = __DIR__ . '/' . $this->context->route() . '-' . $identifier . '.txt';
+
+        if($this->file->exists($path)) {
+            $this->content = $this->file->get($path);
+            return true;
+        }
+
+        return false;
     }
     
     /**
      * renders the program and returns the result
      * @return string
      */
-    public function render(){
-        return "I'm so cool!";
+    public function render() {
+        return $this->content;
     }
 }
