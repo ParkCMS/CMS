4100b884aba1b8af9a5cadf1745037efd6c0f88a
Freeeed <friedrich@ueberreiter.com>
2014-03-25 11:30:32 +0100

    removed programs from default view

--- app/views/page_templates/default.blade.php
+++ app/views/page_templates/default.blade.php
@@ -2,16 +2,4 @@
 
 @section('body')
 <h1>Default</h1>
-
-<div pcms-text="global">
-    Ich bin der Standardtext!
-</div>
-
-<div pcms-cool-ns-program="p1">
-    Cooles Programm p1
-</div>
-
-<div pcms-cool-ns-program="p2">
-    Cooles Programm p2
-</div>
 @stop
