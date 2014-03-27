fba3edb82420788943aafffa571db8859f08f6fb
Freeeed <friedrich@ueberreiter.com>
2014-03-27 10:46:10 +0100

    Added data-aync asset to view

--- app/views/layout.blade.php
+++ app/views/layout.blade.php
@@ -4,8 +4,6 @@
         <meta name="viewport" content="width=device-width, initial-scale=1" />
 
         <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
-
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
         
     	<title>ParkCMS</title>
 	</head>
@@ -56,6 +54,9 @@
             </div>
         </footer>
 
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
         <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+
+        <script src="{{ URL::to('themes/default/js/data-async.js') }}"></script>
 	</body>
 </html>
