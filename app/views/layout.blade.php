8ecf88c117d56e38e4d27c7bdb3feb1411c697ed
Freeeed <friedrich@ueberreiter.com>
2014-03-27 14:11:43 +0100

    Added asset tags to layout

--- app/views/layout.blade.php
+++ app/views/layout.blade.php
@@ -3,6 +3,7 @@
 	<head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
 
+        <pcms-styles />
         <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
         
     	<title>ParkCMS</title>
@@ -53,9 +54,6 @@
                 </div>
             </div>
         </footer>
-
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-        <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
 
         <pcms-scripts />
 	</body>
