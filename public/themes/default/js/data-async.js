6368416834a57e651dfb58a48395fba40928c7b2
Freeeed <friedrich@ueberreiter.com>
2014-03-27 10:44:37 +0100

    Added data-async js package

---
+++ public/themes/default/js/data-async.js
@@ -0,0 +1,32 @@
+
+jQuery(function($) {
+    $('body').on('submit', 'form[data-async]', function(event) {
+        var $form = $(this);
+        var $target = $($form.data('target'));
+        
+        $target.modal('show');
+        $.ajax({
+            type: $form.attr('method'),
+            url: $form.attr('action'),
+            data: $form.serialize(),
+            
+            success: function(data, status) {
+                $target.find('.modal-content').html(data);
+            }
+        });
+        
+        event.preventDefault();
+    });
+
+    $('body').on('click', 'a[data-async]', function(event) {
+        var $a = $(this);
+        var $target = $($a.data('target'));
+        
+        $target.modal('show');
+        $.get($a.attr('href'),function(data, status) {
+            $target.find('.modal-content').html(data);
+        });
+        
+        event.preventDefault();
+    });
+});
