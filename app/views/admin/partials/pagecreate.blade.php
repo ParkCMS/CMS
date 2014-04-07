a8d46ca394e7d936b5624e96bec34ec64fc74fc1
David Bohn <david@cancrisoft.net>
2014-04-07 23:48:09 +0200

    Create form handling on client

--- app/views/admin/partials/pagecreate.blade.php
+++ app/views/admin/partials/pagecreate.blade.php
@@ -1,15 +1,15 @@
-<div class="create-form">
+<div class="create-form" ng-show="visible">
     <h3 ng-if="position == 'child'">Create child of <code>@{{ from.lang }}/@{{ from.alias }}</code></h3>
     <h3 ng-if="position == 'after'">Create sibling after <code>@{{ from.lang }}/@{{ from.alias }}</code></h3>
     <h3 ng-if="position == 'before'">Create sibling before <code>@{{ from.lang }}/@{{ from.alias }}</code></h3>
-    <form role="form">
+    <form role="form" ng-submit="createPage()">
         <div class="form-group">
             <label for="title">Title</label>
-            <input type="text" class="form-control" id="title" placeholder="Title" />
+            <input type="text" class="form-control" required ng-model="new.title" id="title" placeholder="Title" />
         </div>
         <div class="form-group">
             <label for="alias">Alias</label>
-            <input type="text" class="form-control" id="alias" placeholder="Alias" />
+            <input type="text" class="form-control" required ng-model="new.alias" id="alias" placeholder="Alias" />
         </div>
         <div class="form-group">
             <label for="lang">Language</label>
@@ -17,30 +17,30 @@
         </div>
         <div class="form-group">
             <label for="template">Template</label>
-            <input type="text" class="form-control" id="template" ng-model="from.template" />
+            <input type="text" class="form-control" required ng-model="new.template" id="template" ng-model="from.template" />
         </div>
         <div class="form-group">
             <label for="unpublished">Publish State</label>
             <div class="radio">
                 <label>
-                    <input type="radio" name="unpublished" value="0" ng-checked="from.unpublished === 0 || position !== 'child'">
+                    <input type="radio" name="unpublished" ng-true-value="0" ng-model="new.unpublished" value="0" ng-checked="new.unpublished === 0">
                     Published
                 </label>
             </div>
             <div class="radio">
                 <label>
-                    <input type="radio" name="unpublished" value="1" ng-checked="from.unpublished > 0 && position == 'child'">
+                    <input type="radio" name="unpublished" ng-true-value="1" ng-model="new.unpublished" value="1" ng-checked="new.unpublished === 1">
                     Hidden in Menu, but accessable via Link
                 </label>
             </div>
             <div class="radio">
                 <label>
-                    <input type="radio" name="unpublished" value="2">
+                    <input type="radio" name="unpublished" ng-true-value="2" ng-model="new.unpublished" value="2" ng-checked="new.unpublished === 2">
                     Unpublished (i.e. not accessable via link or menu)
                 </label>
             </div>
         </div>
         <button type="submit" class="btn btn-primary">Submit</button>
-        <a href="#" class="btn btn-default" ng-click="cancelCreate()">Cancel</a>
+        <a href="#" class="btn btn-default" ng-click="cancelCreate($event)">Cancel</a>
     </form>
 </div>
--- frontend_src/js/directives/pageCreate.js
+++ frontend_src/js/directives/pageCreate.js
@@ -6,10 +6,35 @@ parkAdmin.directive("pageCreate", ['PagesService', function(PagesService) {
             page: '=',
             from: '=',
             position: '=',
-            visible: '=?'
+            visible: '='
         },
         link: function(scope, element, attributes) {
-            console.log('loaded');
+            scope.new = {};
+            scope.$watch(function() {
+                return scope.from;
+            }, function() {
+                if (scope.position === 'child' && scope.from.unpublished > 0) {
+                    scope.new.unpublished = 1;
+                } else if (scope.position != 'child' && typeof scope.from !== 'undefined') {
+                    scope.new.unpublished = scope.from.unpublished;
+                } else {
+                    scope.new.unpublished = 2;
+                }
+                console.log(scope.new);
+            });
+
+            scope.createPage = function() {
+                PagesService.createPage(scope.new, scope.position, scope.from);
+            };
+
+            scope.cancelCreate = function(event) {
+                var unpublished = scope.new.unpublished;
+                scope.new = {};
+                scope.new.unpublished = unpublished;
+                scope.visible = false;
+
+                event.preventDefault();
+            }
         }
     };
 }]);
