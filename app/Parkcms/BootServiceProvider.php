b3db5c84ee52c497505f53d1af7e717d20f80cbc
Friedrich <friedrich@ueberreiter.com>
2014-04-08 15:05:44 +0200

    Moved Assets to correct place

--- app/Parkcms/BootServiceProvider.php
+++ app/Parkcms/BootServiceProvider.php
@@ -4,6 +4,8 @@ namespace Parkcms;
 
 use Illuminate\Support\ServiceProvider;
 
+use Asset;
+
 class BootServiceProvider extends ServiceProvider {
 
     /**
@@ -31,6 +33,13 @@ class BootServiceProvider extends ServiceProvider {
             'parkcms-views',
             array($this->app['current_theme'], $this->app['default_theme'])
         );
+
+        Asset::add('main', 'themes/' . $theme . '/css/main.css');
+
+        Asset::add('jquery', 'themes/default/js/jquery.min.js');
+        Asset::add('bootstrap', 'themes/default/js/bootstrap.min.js', array('jquery'));
+
+        Asset::add('main', 'themes/default/js/main.js', array('bootstrap'));
     }
 
     /**
