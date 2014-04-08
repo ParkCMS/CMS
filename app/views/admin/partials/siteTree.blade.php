9f5d8a5f6144cedd76ac8d4e24aaf4dac6a8e799
David Bohn <david@cancrisoft.net>
2014-04-08 15:27:34 +0200

    Improved Site tree

--- app/views/admin/partials/siteTree.blade.php
+++ app/views/admin/partials/siteTree.blade.php
@@ -1,8 +1,8 @@
 <div class="site-tree">
     <script type="text/ng-template"  id="tree_item_renderer.html">
-        <i class="expand" ng-show="data.children && !expanded[data.id]" ng-click="toggleSubtree(data.id)">E</i>
-        <i class="expanded" ng-show="data.children && expanded[data.id]" ng-click="toggleSubtree(data.id)">O</i>
-        <i class="page" ng-show="!data.children">P</i>
+        <i class="expand" ng-show="data.children && !expanded[data.id]" ng-click="toggleSubtree(data.id)"></i>
+        <i class="expanded" ng-show="data.children && expanded[data.id]" ng-click="toggleSubtree(data.id)"></i>
+        <i class="page glyphicon glyphicon-file" ng-show="!data.children"></i>
         <a ng-class="{'selected': data.id == selected}" href="#" ng-click="clickedPage(data, $event)">@{{data.title}} (@{{data.lang}})</a>
         <ul ng-show="data.children && expanded[data.id]" ng-init="tmp_lang = data.lang">
             <li ng-init="data.lang = tmp_lang" ng-repeat="data in data.children | orderObjectBy:'lft'" ng-include="'tree_item_renderer.html'"></li>
