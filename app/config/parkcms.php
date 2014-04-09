68319891f7fb391737426bdfe4011039cffc20a8
Friedrich <friedrich@ueberreiter.com>
2014-04-09 11:36:59 +0200

    Added lang to config

--- app/config/parkcms.php
+++ app/config/parkcms.php
@@ -1,5 +1,6 @@
 <?php
 
 return array(
-    'theme' => 'park'
+    'theme' => 'park',
+    'lang'  => 'de'
 );
--- app/controllers/PageController.php
+++ app/controllers/PageController.php
@@ -30,7 +30,7 @@ class PageController extends Controller {
 
         // @TODO: get user language
         if(is_null($lang)) {
-            $lang = 'de';
+            $lang = Config::get('parkcms.lang');
         }
 
         $root = $this->lookupRoot($lang);
