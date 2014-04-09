cb1524653588f3b37e21be58be813f6385265a27
Friedrich <friedrich@ueberreiter.com>
2014-04-09 11:24:35 +0200

    Added ekomi model scopeByContext

--- programs/Parkcms/Ekomi/Models/Ekomi.php
+++ programs/Parkcms/Ekomi/Models/Ekomi.php
@@ -6,4 +6,9 @@ use Illuminate\Database\Eloquent\Model as Eloquent;
 
 class Ekomi extends Eloquent {
     protected $table = 'ekomi';
+
+    public function scopeByContext($query, $lang, $page, $identifier)
+    {
+        return $query->where('identifier', $page . '-' . $identifier);
+    }
 }
