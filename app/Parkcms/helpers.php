77edc9d49594c96dd40a883740f02e83bfe83088
Friedrich <friedrich@ueberreiter.com>
2014-04-10 10:26:08 +0200

    Added current_lang()

--- app/Parkcms/helpers.php
+++ app/Parkcms/helpers.php
@@ -5,3 +5,13 @@ if(!function_exists('theme_path')) {
         return URL::to('themes/' . App::make('current_theme_name') . '/' . $path);
     }
 }
+
+if(!function_exists('current_lang')) {
+    function current_lang($path = '') {
+        if($context = App::make('Parkcms\Context')) {
+            return $context->lang();
+        } else {
+            return Config::get('parkcms.lang');
+        }
+    }
+}
